ec2b380caa43df4fad0f9d8a6ad13e74fc99a837
Tomin-Joy <tominjk007@gmail.com>
2023-09-14 15:23:15 +0530

    [added] preloader

---
+++ js/preloader.js
@@ -0,0 +1,6 @@
+function preloader(){
+    setTimeout(() => {
+        document.getElementById("desktop").classList.toggle("hidden");
+        document.getElementById("preloader").classList.toggle("hidden");
+    }, 5000);
+}
