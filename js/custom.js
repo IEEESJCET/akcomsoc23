d72363cf8f2043bc721079ad5c6a647b778e4413
Tomin-Joy <tominjk007@gmail.com>
2023-09-26 01:13:01 +0530

    [changed] many things

--- js/custom.js
+++ js/custom.js
@@ -30,4 +30,23 @@
   
   })(window.jQuery);
 
+  var acc = document.getElementsByClassName
+    ("accordion")
+  var i;
 
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      this.parentElement.classList.toggle("active");
+
+      var answer = this.nextElementSibling;
+
+      if (answer.style.display === "block") {
+        answer.style.display = "none";
+      }
+      else {
+        answer.style.display = "block";
+      }
+    });
+
+  }
--- js/preloader.js
+++ js/preloader.js
@@ -2,5 +2,5 @@ function preloader(){
     setTimeout(() => {
         document.getElementById("desktop").classList.toggle("hidden");
         document.getElementById("preloader").classList.toggle("hidden");
-    }, 0o0);
+    }, 4000);
 }
