7a78806f01a1add7f43e619f8888e366f77e6fa7
Albin Sony <albinsony2025@cs.sjcetpalai.ac.in>
2023-09-25 23:07:27 +0530

    changed various components

--- js/preloader.js
+++ js/preloader.js
@@ -2,5 +2,5 @@ function preloader(){
     setTimeout(() => {
         document.getElementById("desktop").classList.toggle("hidden");
         document.getElementById("preloader").classList.toggle("hidden");
-    }, 000);
+    }, 0o0);
 }
