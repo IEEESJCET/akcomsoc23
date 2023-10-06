ed235a16723323bce88e8ae4ee7de40eecc74bdc
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-10-07 02:24:36 +0530

    minor changes

--- js/preloader.js
+++ js/preloader.js
@@ -1,5 +1,11 @@
-function preloader(){
-    setTimeout(() => {
-        document.getElementById("preloader").classList.toggle("hidden");
-    }, 4000);
+function preloader() {
+  setTimeout((e) => {
+    document.getElementById("preloader").classList.toggle("hidden");
+    TopScroll = window.pageYOffset || document.documentElement.scrollTop;
+    (LeftScroll = window.pageXOffset || document.documentElement.scrollLeft),
+      // if scroll happens, set it to the previous value
+      (window.onscroll = function () {
+        window.scrollTo(LeftScroll, TopScroll);
+      });
+  }, 4000);
 }
