43f392459d02a541ed42e0fd5acc7934eddd3232
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-10-06 08:01:13 +0530

    preloader error fixed

--- js/preloader.js
+++ js/preloader.js
@@ -1,6 +1,5 @@
 function preloader(){
     setTimeout(() => {
-        document.getElementById("desktop").classList.toggle("hidden");
         document.getElementById("preloader").classList.toggle("hidden");
     }, 4000);
 }
