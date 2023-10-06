c5e410143d624fc2e5b724cbc22e2f1474e92644
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-10-07 02:28:13 +0530

    minor fix

--- js/preloader.js
+++ js/preloader.js
@@ -1,11 +1,12 @@
 function preloader() {
   setTimeout((e) => {
     document.getElementById("preloader").classList.toggle("hidden");
-    TopScroll = window.pageYOffset || document.documentElement.scrollTop;
-    (LeftScroll = window.pageXOffset || document.documentElement.scrollLeft),
-      // if scroll happens, set it to the previous value
-      (window.onscroll = function () {
-        window.scrollTo(LeftScroll, TopScroll);
-      });
+    window.onscroll = function () {};
   }, 4000);
+  TopScroll = window.pageYOffset || document.documentElement.scrollTop;
+  (LeftScroll = window.pageXOffset || document.documentElement.scrollLeft),
+    // if scroll happens, set it to the previous value
+    (window.onscroll = function () {
+      window.scrollTo(0, 0);
+    });
 }
